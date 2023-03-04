6260b2bc67d7f27a81df05c39e2b7b97e453215b
PoomSC <52507079+poomsc@users.noreply.github.com>
2023-03-04 12:45:39 +0700

    map investor registered

--- contracts/DCAmanager.sol
+++ contracts/DCAmanager.sol
@@ -19,6 +19,8 @@ contract DCAManager is IDCAManager {
     /* ============ State Variables ============ */
     address public admin;
     IController public controller;
+    // trustedAddr -> investorAddr -> is investor retgister with this trustedAddr
+    mapping(address => mapping(address => bool)) public investorsRegistered;
     // trustedAddr -> investorAddrs[] -- investroAddr -> invextment
     mapping(address => address[]) public investors;
     mapping(address => IDCAManager.Investment[]) public investments;
@@ -55,6 +57,14 @@ contract DCAManager is IDCAManager {
         uint256 _cycle
     ) external {
         address _investor = msg.sender;
+
+        if (!investorsRegistered[_trustedAddr][_investor]) {
+            address[] storage investorsByTrustedAddr = investors[_trustedAddr];
+            investorsByTrustedAddr.push(_investor);
+            investors[_trustedAddr] = investorsByTrustedAddr;
+            investorsRegistered[_trustedAddr][_investor] = true;
+        }
+
         IDCAManager.Investment[] storage investment = investments[_investor];
         uint256 investmentId = investment.length;
         IDCAManager.Investment memory newInvestment = IDCAManager.Investment(
@@ -67,17 +77,18 @@ contract DCAManager is IDCAManager {
         );
 
         investment.push(newInvestment);
+        investments[_investor] = investment;
+
         buyFor(_investor, investmentId, _indexTokenAmount);
     }
 
     function unsubscription(uint256 _investmentId) external {
         IDCAManager.Investment[] storage investment = investments[msg.sender];
         require(investment.length > _investmentId, "Out of range Investments");
-        for (uint256 i = _investmentId; i < investment.length-1; i++) {
+        for (uint256 i = _investmentId; i < investment.length - 1; i++) {
             investment[i] = investment[i + 1];
         }
         investment.pop();
-
         investments[msg.sender] = investment;
     }
 
@@ -108,7 +119,7 @@ contract DCAManager is IDCAManager {
             investment.tokenIn
         );
 
-console.log("DCA tokenInAmount: %s", tokenInAmount);
+        console.log("DCA tokenInAmount: %s", tokenInAmount);
 
         require(
             IERC20(investment.tokenIn).transferFrom(
